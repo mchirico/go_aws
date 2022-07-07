81894ad8ee0c94d88f74db02337a6a80f7862b76
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2022-07-07 12:20:05 -0400

    update-automated-save

--- 1 Starting Simple - Create S3 Bucket/bucket/bucket.go
+++ 1 Starting Simple - Create S3 Bucket/bucket/bucket.go
@@ -3,9 +3,12 @@ package bucket
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
+	"os"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
@@ -52,7 +55,81 @@ func (b *Bucket) listBuckets(cfg aws.Config, input *s3.ListBucketsInput) (*s3.Li
 	return result, nil
 }
 
-func (b *Bucket) RunDelete(cfg aws.Config) *s3.DeleteBucketOutput {
+func ItemsInBucket(cfg aws.Config, bucket string) ([]string, error) {
+	client := s3.NewFromConfig(cfg)
+	out := []string{}
+	input := &s3.ListObjectsV2Input{
+		Bucket: &bucket,
+	}
+
+	resp, err := getObjects(context.TODO(), client, input)
+	if err != nil {
+		fmt.Println("Got error retrieving list of objects:")
+		fmt.Println(err)
+		return out, err
+	}
+	fmt.Println("\n\nObjects in " + bucket + ":")
+
+	for _, item := range resp.Contents {
+		fmt.Println("Name:          ", *item.Key)
+		fmt.Println("Last modified: ", *item.LastModified)
+		fmt.Println("Size:          ", item.Size)
+		fmt.Println("Storage class: ", item.StorageClass)
+		fmt.Println("")
+		out = append(out, *item.Key)
+	}
+
+	fmt.Println("Found", len(resp.Contents), "items in bucket", bucket)
+	fmt.Println("")
+	return out, nil
+
+}
+
+type s3ListObjectsAPI interface {
+	ListObjectsV2(ctx context.Context,
+		params *s3.ListObjectsV2Input,
+		optFns ...func(*s3.Options)) (*s3.ListObjectsV2Output, error)
+}
+
+func getObjects(c context.Context, api s3ListObjectsAPI, input *s3.ListObjectsV2Input) (*s3.ListObjectsV2Output, error) {
+	return api.ListObjectsV2(c, input)
+}
+
+func (b *Bucket) download(cfg aws.Config, bucket, key string, w io.WriterAt) (int64, error) {
+	client := s3.NewFromConfig(cfg)
+	downloader := manager.NewDownloader(client)
+
+	numBytes, err := downloader.Download(context.TODO(), w, &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+	return numBytes, err
+}
+
+func (b *Bucket) upload(cfg aws.Config, bucket, key string, r io.Reader) (*manager.UploadOutput, error) {
+	client := s3.NewFromConfig(cfg)
+	uploader := manager.NewUploader(client)
+
+	result, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
+		Bucket: &bucket,
+		Key:    aws.String(key),
+		Body:   r,
+	})
+
+	return result, err
+}
+
+func (b *Bucket) Upload(cfg aws.Config, bucket, key, fileToUpload string) (*manager.UploadOutput, error) {
+	f, err := os.Open(fileToUpload)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	return b.upload(cfg, bucket, key, f)
+
+}
+
+func (b *Bucket) Delete(cfg aws.Config) *s3.DeleteBucketOutput {
 	input := &s3.DeleteBucketInput{
 		Bucket: b.Name,
 	}
@@ -63,7 +140,7 @@ func (b *Bucket) RunDelete(cfg aws.Config) *s3.DeleteBucketOutput {
 	return result
 }
 
-func (b *Bucket) RunCreate(cfg aws.Config) {
+func (b *Bucket) Create(cfg aws.Config) {
 	input := &s3.CreateBucketInput{
 		Bucket: b.Name,
 	}
@@ -74,14 +151,14 @@ func (b *Bucket) RunCreate(cfg aws.Config) {
 	fmt.Printf("result: %v", result)
 }
 
-func (b *Bucket) RunList(cfg aws.Config) {
+func (b *Bucket) List(cfg aws.Config) (*s3.ListBucketsOutput, error) {
 	input := &s3.ListBucketsInput{}
 	result, err := b.listBuckets(cfg, input)
 	if err != nil {
-		log.Fatalf("%s", err.Error())
+		return result, err
 	}
 	for _, v := range result.Buckets {
-
 		fmt.Printf("%s, \t\t%s\n", *v.Name, *v.CreationDate)
 	}
+	return result, err
 }
--- 1 Starting Simple - Create S3 Bucket/bucket/bucket_test.go
+++ 1 Starting Simple - Create S3 Bucket/bucket/bucket_test.go
@@ -1,11 +1,89 @@
 package bucket
 
 import (
+	"bytes"
+	"io/ioutil"
 	"lesson1/client"
+	"strconv"
 	"testing"
+
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
+type S3GetObjectAPI interface {
+	GetObject(ctx context.Context, params *s3.GetObjectInput, optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error)
+}
+
+func GetObjectFromS3(ctx context.Context, api S3GetObjectAPI, bucket, key string) ([]byte, error) {
+	object, err := api.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: &bucket,
+		Key:    &key,
+	})
+	if err != nil {
+		return nil, err
+	}
+	defer object.Body.Close()
+
+	return ioutil.ReadAll(object.Body)
+}
+
+type mockGetObjectAPI func(ctx context.Context, params *s3.GetObjectInput, optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error)
+
+func (m mockGetObjectAPI) GetObject(ctx context.Context, params *s3.GetObjectInput, optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error) {
+	return m(ctx, params, optFns...)
+}
+
+func TestGetObjectFromS3(t *testing.T) {
+	cases := []struct {
+		client func(t *testing.T) S3GetObjectAPI
+		bucket string
+		key    string
+		expect []byte
+	}{
+		{
+			client: func(t *testing.T) S3GetObjectAPI {
+				return mockGetObjectAPI(func(ctx context.Context, params *s3.GetObjectInput, optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error) {
+					t.Helper()
+					if params.Bucket == nil {
+						t.Fatal("expect bucket to not be nil")
+					}
+					if e, a := "fooBucket", *params.Bucket; e != a {
+						t.Errorf("expect %v, got %v", e, a)
+					}
+					if params.Key == nil {
+						t.Fatal("expect key to not be nil")
+					}
+					if e, a := "barKey", *params.Key; e != a {
+						t.Errorf("expect %v, got %v", e, a)
+					}
+
+					return &s3.GetObjectOutput{
+						Body: ioutil.NopCloser(bytes.NewReader([]byte("this is the body foo bar baz"))),
+					}, nil
+				})
+			},
+			bucket: "fooBucket",
+			key:    "barKey",
+			expect: []byte("this is the body foo bar baz"),
+		},
+	}
+
+	for i, tt := range cases {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			ctx := context.TODO()
+			content, err := GetObjectFromS3(ctx, tt.client(t), tt.bucket, tt.key)
+			if err != nil {
+				t.Fatalf("expect no error, got %v", err)
+			}
+			if e, a := tt.expect, content; bytes.Compare(e, a) != 0 {
+				t.Errorf("expect %v, got %v", e, a)
+			}
+		})
+	}
+}
 func TestBucket_RunList(t *testing.T) {
 	b := NewBucket()
-	b.RunList(client.Config())
+	b.List(client.Config())
 }
